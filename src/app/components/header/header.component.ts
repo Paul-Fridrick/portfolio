e7929beaa069b3648eb577ab92e31d8f34bd0e60
Paul FRIDRICK <paul.fridrick@epitech.eu>
2024-11-11 11:31:37 +0100

    [ADD] - responsive on each section

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { ViewportScroller} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+
+  constructor(private viewport: ViewportScroller) {}
+
+  scrollToSection(sectionId: string): void {
+    this.isMenuOpen = false;
+    this.viewport.scrollToAnchor(sectionId);
+  }
 
 }
