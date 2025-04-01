23a2aa65644f1a22e8fd21a17d5fa284770fb78f
paul <paul.fridrick@yaakadev.com>
2025-04-01 14:43:46 +0200

    [UPDATE] Ajout du mode sombre

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,8 +10,24 @@ import { ViewportScroller} from '@angular/common';
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  isDarkMode = false;
 
   constructor(private viewport: ViewportScroller) {}
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    this.isDarkMode = savedTheme === 'dark';
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  toggleTheme() {
+    const html = document.documentElement;
+    this.isDarkMode = !html.classList.contains('dark');
+    html.classList.toggle('dark');
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
 
   scrollToSection(sectionId: string): void {
     this.isMenuOpen = false;
